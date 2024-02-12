4721fa39e2f2d6e97718b64d378eed7095f958cb
Vicente Marin <carduks.a@gmail.com>
2024-02-12 10:53:16 -0300

    Correcion modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,11 @@ const routes: Routes = [
   {
     path: '**',
     redirectTo: 'login'
+  },  {
+    path: 'menu-inicio',
+    loadChildren: () => import('./menu-inicio/menu-inicio.module').then( m => m.MenuInicioPageModule)
   }
+
 ];
 
 @NgModule({
